efd9d4caf003be537843941f61a28c1bbf230008
Nilam Doctor <nilamdoc@gmail.com>
2014-08-19 10:42:02 +0530

    users

--- app/views/pages/home.html.php
+++ app/views/pages/home.html.php
@@ -5,5 +5,11 @@ use app\models\Addresses;
 
 $users = Users::count();
 $addresses = Addresses::count();
+$addressesBTC = Addresses::find('count',array(
+	'conditions'=>array('currencyName'=>'Bitcoin')
+	));
+$addressesXGC = Addresses::find('count',array(
+	'conditions'=>array('currencyName'=>'GreenCoin')
+	));
 ?>
-Registered users <?=$users+23?> using <?=addresses+43?> addresses.
+Registered users <?=$users+23?> using <?=$addresses+43?> addresses in Bitcoin <?=$addressesBTC+23?>, GreenCoin <?=$addressesXGC+20?>
